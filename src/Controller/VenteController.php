7a838958b30ba59f481d7be1683c97c05561118d
asonkes <audrey.sonkes@gmail.com>
2023-12-03 20:42:52 +0100

    création VenteController

---
+++ src/Controller/VenteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VenteController extends AbstractController
+{
+    #[Route('/vente', name: 'vente')]
+    public function index(): Response
+    {
+        return $this->render('vente/index.html.twig', []);
+    }
+}
