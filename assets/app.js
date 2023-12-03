1b68f515a2da2818403cf10bb1c8b49919ed7c2d
asonkes <audrey.sonkes@gmail.com>
2023-12-03 20:25:52 +0100

    HomeController

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,6 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import "./styles/bootstrap.min.css";
+
+import "bootstrap/dist/js/bootstrap.bundle";
