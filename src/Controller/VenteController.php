5af8b9460ea25e350e4a20afe7ae01975726a589
asonkes <audrey.sonkes@gmail.com>
2023-12-03 21:55:56 +0100

    creation du 1er formulaire - AnnonceType

--- src/Controller/VenteController.php
+++ src/Controller/VenteController.php
@@ -4,15 +4,18 @@ namespace App\Controller;
 
 use App\Entity\Voiture;
 use App\Repository\VoitureRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class VenteController extends AbstractController
 {
     /**
-     * Permet de pouvoir créer des annonces, de pouvoir avoir accès à la page "Vente"
+     * Permet de pouvoir mettre des annonces
      *
+     * @param VoitureRepository $repo
      * @return Response
      */
     #[Route('/vente', name: 'vente')]
@@ -21,18 +24,77 @@ class VenteController extends AbstractController
         // On met "voitures" au pluriel pour pouvoir récupérer toutes les voitures
         $voitures = $repo->findAll();
         return $this->render('vente/index.html.twig', [
-            'voitures' => $voitures
+            'voitures' => $voitures,
         ]);
     }
 
-    #[route('vente/{slug}', name: "vente_show")]
+    #[Route('/vente/new', name: 'new')]
+    public function create(Request $request, EntityManagerInterface $manager): Response
+    {
+        $voiture = new Voiture();
+        $myForm = $this->createForm(AnnonceType::class, $voiture);
+
+        $myForm->handleRequest($request);
+
+        if ($myForm->isSubmitted() && $myForm->isValid()) {
+            $manager->persist($voiture);
+
+            # j'envoie les persistances à la base de données.
+            $manager->flush();
+
+            # Permet d'avoir un message "Flash", d'où "addFlash" pour dire que l'annonce a bien été enregistrée !!!
+            $this->addFlash('success', "L'annonce <strong>" . $voiture->getMarque() . "</strong> a bien été enregistrée");
+
+            /**
+             * Redirige si par exemple, l'annonce a bien été enregistrée, je retourne vars la page "show".
+             */
+            return $this->redirectToRoute('new');
+        }
+
+        return $this->render('vente/new.html.twig', [
+            'myForm' => $myForm->createView()
+        ]);
+    }
+
+    #[Route("/vente/{id}/edit", name: 'edit')]
+    public function edit(Request $request, EntityManagerInterface $manager, Voiture $voiture): Response
+    {
+        $form = $this->createForm(AnnonceType::class, $voiture);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // gestion des images 
+            foreach ($voiture->getImages() as $image) {
+                $image->setVoiture($voiture);
+                $manager->persist($image);
+            }
+
+            $manager->persist($voiture);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>" . $voiture->getMarque() . "</strong> a bien été modifiée!"
+            );
+
+            return $this->redirectToRoute('vente');
+        }
+
+        return $this->render("vente/edit.html.twig", [
+            "fleur" => $voiture,
+            "myForm" => $form->createView()
+        ]);
+    }
+
+    #[Route('/vente/{slug}', name: 'vente_show')]
     public function show(voiture $voiture): Response
     {
+        /**
+         * $images = $voiture->getImages(); Ici je fais appel à la méthode getImages sur l'objet voiture et que j'attends d'avoir des images reliées à cet objet voiture...
+         */
         $images = $voiture->getImages();
         return $this->render('vente/show.html.twig', [
-            /**
-             * $images = $voiture->getImages(); Ici je fais appel à la méthode getImages sur l'objet voiture et que j'attends d'avoir des images reliées à cet objet voiture...
-             */
             'voiture' => $voiture,
             // Ici, on met "image" au pluriel car on a plusieurs images (inverse que sur index.html.twig)
             'images' => $images
---
+++ src/Form/AnnonceType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Voiture;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnnonceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('marque')
+            ->add('modele')
+            ->add('cover')
+            ->add('slug')
+            ->add('km')
+            ->add('price')
+            ->add('nbProprio')
+            ->add('cylindree')
+            ->add('puissance')
+            ->add('carburant')
+            ->add('date')
+            ->add('transmission')
+            ->add('description')
+            ->add('texte')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Voiture::class,
+        ]);
+    }
+}
