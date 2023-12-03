813cb308871466f48047faf417c71d127d92f7a2
asonkes <audrey.sonkes@gmail.com>
2023-12-03 21:24:50 +0100

    pages index et show.html.twig en ordre - manque carroussel

--- src/Controller/VenteController.php
+++ src/Controller/VenteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Voiture;
 use App\Repository\VoitureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,6 +22,20 @@ class VenteController extends AbstractController
         $voitures = $repo->findAll();
         return $this->render('vente/index.html.twig', [
             'voitures' => $voitures
+        ]);
+    }
+
+    #[route('vente/{slug}', name: "vente_show")]
+    public function show(voiture $voiture): Response
+    {
+        $images = $voiture->getImages();
+        return $this->render('vente/show.html.twig', [
+            /**
+             * $images = $voiture->getImages(); Ici je fais appel à la méthode getImages sur l'objet voiture et que j'attends d'avoir des images reliées à cet objet voiture...
+             */
+            'voiture' => $voiture,
+            // Ici, on met "image" au pluriel car on a plusieurs images (inverse que sur index.html.twig)
+            'images' => $images
         ]);
     }
 }
